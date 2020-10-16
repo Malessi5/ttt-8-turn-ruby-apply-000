87120b5b1e773b17293ee60d7b1020c2548ec79a
Mike Alessi <mikealessi5@gmail.com>
2020-10-16 18:10:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board,index)
-if index == (0||1||2||3||4||5||6||7||8) && !position_taken?(board,index)
+if index == ( 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 ) && !position_taken?(board,index)
   return true
 end
 end
